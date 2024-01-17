cc72e3036ec665389207d382277a9705653a1c97
rsalvatori <rsalvatori@perforce.com>
2024-01-17 16:40:48 +0100

    bugfix session url

--- app/code/local/Schracklive/Typo3/Helper/SessionUrl.php
+++ app/code/local/Schracklive/Typo3/Helper/SessionUrl.php
@@ -6,7 +6,7 @@ class Schracklive_Typo3_Helper_SessionUrl
     public function replacePlaceholderUrls($html)
     {
         // Replace placeholders with magento URLs
-        preg_match_all('/<a.*[^>]*href=(\"??)(shop:\/\/[^\" >]*?)\\1[^>]*>(.*)/siU', $html, $matches);
+        preg_match_all('/<a.*[^>]*href=(\"??)(shop:\/\/[^\" >]*?)\\1[^>]*>(.*)/siU', (string) $html, $matches);
         $urls = array_unique($matches[2]);
         foreach ($urls as $url) {
             $html = str_ireplace($url, Mage::getUrl((str_replace('shop://', '', strtolower($url)))), $html);
